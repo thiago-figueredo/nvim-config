6aa7c3d6caf7abf04cfc3a134f8045c19477a53b
thiago-figueredo <thiago16fca@outlook.com>
2024-06-15 11:50:28 -0300

    feat: remove unused comments, and add plugin name in keymap description to help find keymaps of a given plugin

--- lua/thiago/plugins/gitsigns.lua
+++ lua/thiago/plugins/gitsigns.lua
@@ -13,11 +13,5 @@ return {
 				virt_text_priority = 100,
 			},
 		})
-
-		local function map(mode, l, r, opts)
-			opts = opts or {}
-			opts.buffer = vim.api.nvim_get_current_buf()
-			vim.keymap.set(mode, l, r, opts)
-		end
 	end,
 }
