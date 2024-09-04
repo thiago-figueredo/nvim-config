0658412a89fb90b386ef72fc4edf6a394cd94787
Thiago Figueredo da Costa Antunes <thiago16fca@outlook.com>
2024-09-04 08:52:01 -0300

    bugfix: on VimEnter event open harpoon buffers even though the opened files are not in the directory of harpoon buffers

--- lua/thiago/core/autocmds.lua
+++ lua/thiago/core/autocmds.lua
@@ -37,10 +37,30 @@ vim.api.nvim_create_autocmd({ "VimEnter" }, {
     vim.schedule(function()
       local harpoon = require("harpoon")
       local harpoon_items = harpoon:list().items
+      local neovim_args = vim.fn.argv()
+      local filepath = neovim_args[1]
+
+      if not filepath then
+        return
+      end
+
+      local _, end_idx = filepath:find("://")
+
+      if not end_idx then
+        return
+      end
+
+      filepath = filepath:sub(end_idx + 1)
+
+      if filepath ~= vim.fn.getcwd() .. '/' then
+        return
+      end
 
       for _, item in ipairs(harpoon_items) do
-        vim.cmd('edit ' .. item.value)
-        vim.api.nvim_win_set_cursor(0, { item.context.row, item.context.col })
+        if io.open(item.value) ~= nil then
+          vim.cmd('edit ' .. item.value)
+          vim.api.nvim_win_set_cursor(0, { item.context.row, item.context.col })
+        end
       end
     end)
   end,
