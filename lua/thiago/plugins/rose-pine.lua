844d06c02e512419a073502bf2ac6c8c5b75f1c9
Thiago Figueredo da Costa Antunes <thiago16fca@outlook.com>
2024-11-20 10:41:22 -0300

    feat: overwrite rose-pine highlight groups

--- lua/thiago/plugins/rose-pine.lua
+++ lua/thiago/plugins/rose-pine.lua
@@ -1,15 +1,24 @@
 return {
   "rose-pine/neovim",
   config = function()
+    vim.api.nvim_create_autocmd("ColorScheme", {
+      pattern = "rose-pine-moon",
+      callback = function()
+        vim.api.nvim_set_hl(0, "@function.method.call", { fg = "#ea9a97" })
+      end,
+    })
+
     require("rose-pine").setup({
       variant = "main",
       dim_inactive_windows = true,
       extend_background_behind_borders = true,
-
       enable = {
         terminal = true,
       },
-
+      highlight_groups = {
+        ["@variable.parameter"] = { fg = "#f9cc9d" },
+        ["@function.method.call"] = { fg = "#ebbcba" },
+      },
       styles = {
         bold = true,
         italic = false,
